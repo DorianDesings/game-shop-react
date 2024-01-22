c34d755229ece03c1836c91b98403d643193d778
Dorian <dorianoriginaldesings@gmail.com>
2024-01-22 06:56:50 +0100

    sortBy interface

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,6 +1,7 @@
 import { useFilters } from '../../hooks/useFilters';
 import { getPlattforms } from '../../utils/get-platforms';
 import FilterPlatform from '../filter-platform/FilterPlatform';
+import SortByGames from '../sort-by-games/SortByGames';
 
 const Filters = () => {
 	const { filters, changeFilter } = useFilters();
@@ -16,6 +17,8 @@ const Filters = () => {
 					changeFilter={checked => changeFilter(platform, checked)}
 				/>
 			))}
+			<h2>Sort By</h2>
+			<SortByGames />
 		</div>
 	);
 };
---
+++ src/components/sort-by-games/SortByGames.jsx
@@ -0,0 +1,13 @@
+const SortByGames = () => {
+	return (
+		<div>
+			<select name=''>
+				<option value={0}>Default</option>
+				<option value={1}>Name</option>
+				<option value={2}>Price</option>
+			</select>
+		</div>
+	);
+};
+
+export default SortByGames;
